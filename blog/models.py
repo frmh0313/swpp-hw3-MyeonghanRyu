eb87c41ef865604e0e9e6dd03445680e99f9886d
frmh0313 <rmh0313@gmail.com>
2017-11-02 01:56:21 +0900

    implemented initial model

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=16)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    author_id = models.IntegerField()
+
+
+class Comment(models.Model):
+    article_id = models.IntegerField()
+    content = models.TextField()
+    author_id = models.IntegerField()
