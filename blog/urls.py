052938d4062c437b1b19bc914321cdbeba6b3c46
frmh0313 <rmh0313@gmail.com>
2017-11-03 01:00:40 +0900

    urls first draft

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from blog import views
 urlpatterns = [
     url('^signup$', views.signup, name='signup'),
     url('^token$', views.token, name='token'),
+    url('^article$', views.articleList, name='articleList'),
+    url('^article/(?P<article_id>[0-9]+)$', views.articleDetail, name='articleDetail'),
+    url('^article/(?P<article_id>[0-9]+)/comment', views.commentList, name='commentList'),
+    url('^comment/(?P<comment_id>[0-9]+)$', views.commentDetail, name='commentDetail'),
 ]
