fe70e9c6ded0f3815a23b061099ee3474c12b07e
frmh0313 <rmh0313@gmail.com>
2017-11-03 01:00:53 +0900

    views first draft

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponseNotFound
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.forms.models import model_to_dict
+from .models import Article, User, Comment
 import json
 
 
@@ -21,3 +24,108 @@ def token(request):
         return HttpResponse(status=204)
     else:
         return HttpResponseNotAllowed(['GET'])
+
+
+def articleList(request):
+    if request.method == 'GET':
+        return JsonResponse(list(Article.objects.all().values()), safe=False)
+    elif request.method == 'POST':
+        title = json.loads(request.body.decode())['title']
+        content = json.loads(request.body.decode())['content']
+        author_id = json.loads(request.body.decode())['author_id']
+        new_article = Article(title=title, content=content, author_id=author_id)
+        new_article.save()
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+
+def articleDetail(request, article_id):
+    article_id = int(article_id)
+    if request.method == 'GET':
+        try:
+            article = Article.objects.get(id=article_id)
+        except Article.DoesNotExist:
+            return HttpResponseNotFound()
+        return JsonResponse(model_to_dict(article))
+    elif request.method == 'PUT':
+        title = json.loads(request.body.decode())['title']
+        content = json.loads(request.body.decode())['content']
+        author_id = json.loads(request.body.decode())['author_id']
+        try:
+            article = Article.objects.get(id=article_id)
+        except Article.DoesNotExist:
+            return HttpResponseNotFound()
+        article.title = title
+        article.content = content
+        article.author_id = author_id
+        article.save()
+        return HttpResponse(status=204)
+    elif request.method == 'DELETE':
+        try:
+            article = Article.objects.get(id=article_id)
+        except Article.DoesNotExist:
+            return HttpResponseNotFound()
+        article.delete()
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
+
+
+def commentList(request, article_id):
+    article_id = int(article_id)
+    if request.method == 'GET':
+        try:
+            comments = Article.objects.get(id=article_id).comments.all() #type of comments? QuerySet? List?
+            print(type(comments))
+        except Comment.DoesNotExist:
+            return HttpResponseNotFound()
+        return JsonResponse(list(comments), safe=False)
+    elif request.method == 'POST':
+        content = json.loads(request.body.decode())['content']
+        author_id = json.loads(request.body.decode())['author_id']
+        new_comment = Comment(article_id=article_id, content=content, author_id=author_id,
+                              article=Article.objects.get(id=article_id))
+        new_comment.save()
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+
+def commentDetail(request, comment_id):
+    comment_id = int(comment_id)
+    if request.method == 'GET':
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return HttpResponseNotFound()
+        return JsonResponse(model_to_dict(comment))
+    elif request.method == 'PUT':
+        # article_id = json.loads(request.body.decode())['article_id']
+        content = json.loads(request.body.decode())['content']
+        author_id = json.loads(request.body.decode())['author_id']
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return HttpResponseNotFound()
+        # comment.article_id = article_id # article modifying not allowed
+        comment.content = content
+        # comment.author_id = author_id # author_id modifying not allowed
+        comment.save()
+        return HttpResponse(status=204)
+    elif request.method == 'DELETE':
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return HttpResponseNotFound()
+        comment.delete()
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
+
+
+
+
+
+
+
