83de1f3c29195838e62f31a1388dd68dc04f2a81
frmh0313 <rmh0313@gmail.com>
2017-11-03 17:21:18 +0900

    test coverage 100%, need to do login_required

--- blog/tests.py
+++ blog/tests.py
@@ -7,9 +7,9 @@ import json
 class BlogTestCase(TestCase):
 
     def setUp(self):
-        User.objects.create(username='User1', password='user1pwd')
-        User.objects.create(username='User2', password='user2pwd')
-        User.objects.create(username='User3', password='user3pwd')
+        User.objects.create_user(username='User1', password='user1pwd')
+        User.objects.create_user(username='User2', password='user2pwd')
+        User.objects.create_user(username='User3', password='user3pwd')
 
         Article.objects.create(title='article1', content='article1 content', author=User.objects.get(id=1))
         Article.objects.create(title='article2', content='article2 content', author=User.objects.get(id=1))
@@ -84,23 +84,154 @@ class BlogTestCase(TestCase):
     def test_article_detail_get(self):
         response = self.client.get('/api/article/1')
         loaded_data = json.loads(response.content.decode())
-        data = {'author_id': 1, 'content': 'article1 content', 'id': 1, 'title': 'article1'}
+        data = {'author': 1, 'content': 'article1 content', 'id': 1, 'title': 'article1'}
         self.assertEqual(loaded_data, data)
         self.assertEqual(response.status_code, 200)
 
+    def test_article_list_get_not_found(self):
+        response = self.client.get('/api/article/5')
+        self.assertEqual(response.status_code, 404)
+
     def test_article_detail_put(self):
         response = self.client.put('/api/article/1',
-                                   json.dumps({'author_id':1, 'content': 'article1 modified content', 'id': 1, 'title': 'article1 modified'}),
+                                   json.dumps(
+                                       {'author_id': 1, 'content': 'article1 modified content',
+                                        'id': 1, 'title': 'article1 modified'}),
                                    content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_detail_put_not_found(self):
+        response = self.client.put('/api/article/5',
+                                   json.dumps(
+                                       {'author_id': 1, 'content': 'article5 fake',
+                                        'id': 5, 'title': 'article5 put'}),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 404)
 
     def test_article_detail_post(self):
-        response = self.client.put('/api/article/1', data=[])
+        response = self.client.post('/api/article/1',
+                                    json.dumps(
+                                       {'author_id': 1, 'content': 'fake article1',
+                                        'id': 1, 'title': 'article1 post'}),
+                                    content_type='application/json')
         self.assertEqual(response.status_code, 405)
 
     def test_article_detail_delete(self):
         response = self.client.delete('/api/article/1')
         self.assertEqual(response.status_code, 200)
+
+    def test_article_detail_delete_not_found(self):
+        response = self.client.delete('/api/article/5')
+        self.assertEqual(response.status_code, 404)
+
+    def test_comment_list_get(self):
+        response = self.client.get('/api/article/1/comment')
+        loaded_data = json.loads(response.content.decode())
+        data = [{'id': 1, 'content': 'comment1', 'author_id': 2, 'article_id': 1},
+                {'id': 2, 'content': 'comment2', 'author_id': 1, 'article_id': 1}]
+        self.assertEqual(loaded_data, data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_comment_list_post(self):
+        response = self.client.post('/api/article/1/comment',
+                                    json.dumps({'content': 'comment4', 'author_id': 1, 'article_id': 1}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_comment_list_put(self):
+        response = self.client.put('/api/article/1/comment',
+                                   json.dumps({'content': 'comment5', 'author_id':1, 'article_id': 1}),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 405)
+
+    def test_comment_list_delete(self):
+        response = self.client.delete('/api/article/1/comment')
+        self.assertEqual(response.status_code, 405)
+
+    def test_comment_detail_get(self):
+        response = self.client.get('/api/comment/1')
+        loaded_data = json.loads(response.content.decode())
+        data = {'id': 1, 'content': 'comment1', 'author': 2, 'article': 1}
+        self.assertEqual(loaded_data, data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_comment_detail_get_not_found(self):
+        response = self.client.get('/api/comment/5')
+        self.assertEqual(response.status_code, 404)
+
+    def test_comment_detail_put(self):
+        response = self.client.put('/api/comment/1',
+                                   json.dumps({'content': 'comment1 modified', 'author_id': 1, 'article_id': 1}),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_comment_detail_put_not_found(self):
+        response = self.client.put('/api/comment/5',
+                                   json.dumps({'content': 'comment5 modified', 'author_id': 1, 'article_id': 1}),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+    def test_comment_detail_delete(self):
+        response = self.client.delete('/api/comment/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_comment_detail_delete_not_found(self):
+        response = self.client.delete('/api/comment/5')
+        self.assertEqual(response.status_code, 404)
+
+    def test_comment_detail_post(self):
+        response = self.client.post('/api/comment/4',
+                                    json.dumps({'content': 'comment4', 'author_id':1, 'article_id': 1}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 405)
+
+    def test_signin_post_valid_user(self):
+        username = 'User1'
+        password = 'user1pwd'
+        response = self.client.post('/api/signin',
+                                    json.dumps({'username': username, 'password': password}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signin_post_invalid_user(self):
+        username = 'User4'
+        password = 'user4pwd'
+        response = self.client.post('/api/signin',
+                                    json.dumps({'username': username, 'password': password}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+
+    def test_signin_get_not_allowed(self):
+        response = self.client.get('/api/signin')
+        self.assertEqual(response.status_code, 405)
+
+    def test_signin_put_not_allowed(self):
+        username = 'User1'
+        password = 'user1pwd'
+        response = self.client.put('/api/signin',
+                                   json.dumps({'username': username, 'password': password}),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 405)
+
+    def test_signin_delete_not_allowed(self):
+        response = self.client.delete('/api/signin')
+        self.assertEqual(response.status_code, 405)
+
+    def test_signout_post(self):
+        response = self.client.post('/api/signout',
+                                    json.dumps({'username': 'User1', 'password':'user1pwd'}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signout_get_not_allowed(self):
+        response = self.client.get('/api/signout')
+        self.assertEqual(response.status_code, 405)
+
+    def test_signout_put_not_allowed(self):
+        response = self.client.put('/api/signout',
+                                   json.dumps({'username': 'User1', 'password':'user1pwd'}),
+                                              content_type='application/json')
+        self.assertEqual(response.status_code, 405)
 
 
 
